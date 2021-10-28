eb663acd53261214ef7f4f3bf92c0176751bc196
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-28 19:35:57 -0400

    base set up w/ components

---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/Song.module.css";
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Name</h1>
+      <h1>Artist</h1>
+      <h1>Name</h1>
+    </div>
+  );
+};
+
+export default Song;
