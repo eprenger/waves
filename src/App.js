24fedfb932e5db39096f467d7d0af87d2fbcd177
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-28 20:55:04 -0400

    imported songs to UI
    added play/pause feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 import data from "./util";
 import "./style/app.scss";
+import { useState } from "react";
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
